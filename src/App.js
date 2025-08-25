8cda2e630f2002d29029ce84e1a0da24da21b588
Ayush Garg <108256486+TAGisON@users.noreply.github.com>
2025-08-25 16:11:16 +0530

    added role based dashboard view

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import DashboardOfficer from "./pages/DashboardOfficer";
 import IncomingQueries from "./pages/Queries/IncomingQueries";
 import RepliedQueries from "./pages/Queries/RepliedQueries";
 import TransferredQueries from "./pages/Queries/TransferredQueries";
@@ -15,6 +16,44 @@ import Iqmsmsi from "./pages/IQMSMSI";
 import ProfileView from "./pages/ProfileView/ProfileView";
 import FreqQuery from "./pages/FreqQuery";
 import DashboardLayout from "./layouts/DashboardLayout";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+
+function AppRoutes() {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <DashboardLayout />
+          </ProtectedRoute>
+        }
+      >
+        {/* ✅ Conditionally render based on role */}
+        <Route
+          index
+          element={
+            user?.role === "officer" ? <DashboardOfficer /> : <Dashboard />
+          }
+        />
+        <Route path="search-query" element={<SearchQuery />} />
+        <Route path="comparision" element={<Comparision />} />
+        <Route path="iqms-mis" element={<Iqmsmsi />} />
+        <Route path="view/queries/incoming" element={<IncomingQueries />} />
+        <Route path="freq-query" element={<FreqQuery />} />
+        <Route path="view/queries/transferred" element={<TransferredQueries />} />
+        <Route path="view/queries/replied" element={<RepliedQueries />} />
+        <Route path="view/query/:id" element={<QueryView />} />
+        <Route path="view/profile" element={<ProfileView />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
 
 function App() {
   document.body.style.backgroundColor = "#f4f6f9";
@@ -22,29 +61,7 @@ function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <DashboardLayout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<Dashboard />} />
-            <Route path="search-query" element={<SearchQuery />} />
-            <Route path="comparision" element={<Comparision />} />
-            <Route path="iqms-mis" element={<Iqmsmsi />} />
-            <Route path="view/queries/incoming" element={<IncomingQueries />} />
-            <Route path="freq-query" element={<FreqQuery />} />
-            <Route path="view/queries/transferred" element={<TransferredQueries />} />
-            <Route path="view/queries/replied" element={<RepliedQueries />} />
-            <Route path="view/query/:id" element={<QueryView />} />
-            <Route path="view/profile" element={<ProfileView />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <AppRoutes />
       </BrowserRouter>
     </AuthProvider>
   );
