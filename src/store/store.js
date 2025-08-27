50d83a4c1bfb746084922cfe139d2c4de676f86b
Rahul singh <100202443+Rahulcse79@users.noreply.github.com>
2025-08-27 16:57:13 +0530

    fix

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,5 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
 import { allReducer } from "../reducers/allReducers";
 
 const initialState = {
@@ -16,10 +15,13 @@ const reducer = combineReducers({
 
 const middleware = [thunk];
 
+// ✅ use Redux DevTools if available, else fallback to Redux compose
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   reducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 export default store;
