6acb45e3359a9fbc1ba57302bef87efd6b2a3ce9
Rahul singh <100202443+Rahulcse79@users.noreply.github.com>
2025-09-16 12:59:00 +0530

    final

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,17 +1,28 @@
-import { Navigate } from "react-router-dom";
-import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../context/AuthContext";
 
 const ProtectedRoute = ({ children, allowedRoles }) => {
-  const { auth } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const { auth, isAuthLoading } = useContext(AuthContext);
 
-  if (!auth) {
-    return <Navigate to="/login" replace />;
-  }
+  // Wait until auth is loaded from cookies
+  useEffect(() => {
+    if (!isAuthLoading) {
+      if (!auth) {
+        navigate("/login", { replace: true });
+        window.location.reload();
+      } else if (
+        allowedRoles &&
+        !auth.user.roles.some((role) => allowedRoles.includes(role))
+      ) {
+        navigate("/unauthorized", { replace: true });
+      }
+    }
+  }, [auth, isAuthLoading, allowedRoles, navigate]);
 
-  if (allowedRoles && !auth.user.roles.some(role => allowedRoles.includes(role))) {
-    return <Navigate to="/unauthorized" replace />;
-  }
+  // Render nothing until auth is loaded or redirect happens
+  if (!auth || isAuthLoading) return null;
 
   return children;
 };
